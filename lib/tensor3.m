7dc3dc1c97d5890112f118c384d27b0d28d75bb9
Songyuan Cui <cui20@illinois.edu>
2023-09-21 21:57:25 -0500

    doc: append previous version of tensor3.m to the same file

--- lib/tensor3.m
+++ lib/tensor3.m
@@ -14,3 +14,10 @@ F=reshape(F,nx,ny,nz);
 for k=1:nz; G(:,:,k) = F(:,:,k)*Ay'; end;
 
 
+% function F = tensor3(Az,Ay,Ax,F);
+
+% [nx,ny,nz]=size(F);
+
+% F=reshape(F,nx,ny*nz); F=Ax*F;
+% F=reshape(F,nx*ny,nz); F=F*Az';
+% F=reshape(F,nx,ny,nz); for k=1:nz; F(:,:,k) = F(:,:,k)*Ay'; end;
