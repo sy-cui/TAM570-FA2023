872ad2b967dae521a686dba80b2f8e74ec3e9ab1
Songyuan Cui <cui20@illinois.edu>
2023-09-29 20:36:22 -0500

    refactor: add surface plot to HW3 poisson 2d q5

--- homework/HW3/poisson_2d_q5.m
+++ homework/HW3/poisson_2d_q5.m
@@ -18,10 +18,21 @@ for i = 1:length(ns);
     max_diffs(i) = abs(max(max(abs(ub))) - max(max(abs(u2b))));
 end;
 
-figure('Units', 'inches', 'Position', [0 0 4 4])
-    semilogy(ns, max_diffs, '-k', lw, 2)
+figure('Units', 'inches', 'Position', [0 0 9 4])
+    subplot(1, 2, 2)
+    loglog(ns, max_diffs, '-ok', lw, 2)
     xlabel('$N$', intp, ltx); 
     ylabel('$L_\infty$ error', intp, ltx);
+
+    subplot(1, 2, 1)
+    n_plot = 32;
+    [x_grid, y_grid, ub] = poisson2d(n_plot, n_plot, bc, rhs_func);
+    surf(x_grid, y_grid, ub)
+    pbaspect([1 1 1])
+    colorbar
+    xlabel('$x$', intp, ltx); 
+    ylabel('$y$', intp, ltx);
+    zlabel('$u(x, y)$', intp, ltx);
     pos = get(gcf, 'Position');
     set(gca, fs, 12, lw, 1, 'fontname', 'serif')
     set(gcf,'PaperUnits', 'inches','PaperSize', [pos(3), pos(4)])
